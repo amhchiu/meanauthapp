3d92a3f90c7d1ea464bb35896706cb74e175b617
amhchiu <physicspianist@gmail.com>
2017-08-12 16:57:19 +0100

    created Register form page with flash messages. Need to learn how to validate field dynamically onchange

--- angular-src/src/app/components/register/register.component.ts
+++ angular-src/src/app/components/register/register.component.ts
@@ -1,15 +1,41 @@
 import { Component, OnInit } from '@angular/core';
+import { ValidateService } from '../../services/validate.service';
+import { FlashMessagesService } from 'angular2-flash-messages';
 
 @Component({
-  selector: 'app-register',
-  templateUrl: './register.component.html',
-  styleUrls: ['./register.component.css']
+    selector: 'app-register',
+    templateUrl: './register.component.html',
+    styleUrls: ['./register.component.css']
 })
 export class RegisterComponent implements OnInit {
+    name: string;
+    username: string;
+    email: string;
+    password: string;
 
-  constructor() { }
+    constructor(private validateService: ValidateService, private flashMessage: FlashMessagesService) { }
 
-  ngOnInit() {
-  }
+    ngOnInit() {
+    }
 
+    onRegisterSubmit(){
+        const user = {
+            name: this.name,
+            email: this.email,
+            username: this.username,
+            password: this.password
+        };
+
+        //Required fields
+        if(!this.validateService.validateRegister(user)){
+            this.flashMessage.show('Please fill in all fields!', {cssClass: 'alert-danger', timeout: 3000});
+            return false;
+        }
+
+        if(!this.validateService.validateEmail(user.email)){
+            this.flashMessage.show('Please use a valid email', {cssClass: 'alert-danger', timeout: 3000});
+            return false;
+        }
+
+    }
 }
